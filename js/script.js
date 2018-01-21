31b1da4320d6cf513554d731eb15a16bbc74785e
welbinator <james.welbes@gmail.com>
2018-01-21 13:46:30 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-                $('.portfolio-image').hover(function() { 
+                $('.portfolio-image').hover(function() {  // overlay, and button appear on hover
                   $(this).find('img').css("opacity","0.5")
                   $(this).find('button').toggle();
                 }, function() { 
@@ -9,10 +9,10 @@
                 
             
         
-            $('#shannon').click(function() { $('#shannon-modal').css("display","flex") });
+            $('#shannon').click(function() { $('#shannon-modal').css("display","flex") }); // opens popup modal on click
             $('#cloud-solutions').click(function() { $('#cloud-solutions-modal').css("display","flex") });
             $('#corridor').click(function() { $('#corridor-modal').css("display","flex") });
             $('#chromebook-guide').click(function() { $('#chromebook-guide-modal').css("display","flex") });
             
-            $('.close').click(function() { $('.portfolio-modal').css("display","none") });
+            $('.close').click(function() { $('.portfolio-modal').css("display","none") }); // closes popup modal on click
             
